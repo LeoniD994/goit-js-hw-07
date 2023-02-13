85139f0eaaa928a0b130583e4bb955df6211d430
Leonid Zelov <112327440+LeonidZelov@users.noreply.github.com>
2023-02-13 15:51:54 +0200

    task 1;task2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,51 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEL = document.querySelector('.gallery');
+
+const imgListTemplate = ({preview, original, description}) => {
+    return `  <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"/>
+            </a>`;
+};
+
+const addImg = galleryItems.map(imgListTemplate).join('');
+galleryEL.insertAdjacentHTML("afterbegin", addImg);
+
+
+
+galleryEL.addEventListener('click', oneGalleryImgClick );
+
+function oneGalleryImgClick (evt){
+    const imageSelected = evt.target.getAttribute("data-source");
+
+
+    evt.preventDefault();
+
+    if (!imageSelected){return;}
+
+
+    const openModulWindow = basicLightbox.create(
+        `<img src="${imageSelected}" width="800" height="600">`,
+    {
+        
+        onShow: () => {document.addEventListener("keydown", closeModal);},
+        
+        onClose: () => {document.removeEventListener("keydown", closeModal);},
+    }
+    );
+
+    openModulWindow.show();
+
+function closeModal(evt) {
+    if (evt.key === "Escape") {
+        openModulWindow.close();
+    }
+}
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const imagesListTemplate = ({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image"
+    src="${preview}" 
+    title="${description}" 
+    alt="${description}" />
+    </a>`;
+};
+
+const addImg = galleryItems.map(imagesListTemplate).join('');
+galleryEl.insertAdjacentHTML("afterbegin", addImg);
+
+
+
+galleryEl.addEventListener('click', galleryImgClick );
+
+function galleryImgClick (evt){
+    const imageSelected = evt.target.getAttribute("data-source");
+
+    evt.preventDefault();
+
+    if (!imageSelected){return;}
+
+};
+
+new SimpleLightbox(".gallery a", {captionDelay: 250, showCounter:false});
